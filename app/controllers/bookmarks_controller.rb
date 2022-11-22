5d724eaf25b882a5053eefb6b904a0f7ad8af86b
Henry Yeh <heyeh@icloud.com>
2022-11-22 19:58:45 +0800

    bookmarks form works but won't save

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,15 +1,22 @@
 class BookmarksController < ApplicationController
   def new
-    @bookmark = Bookmark.new
+    @movies = Movie.all
+    @bookmark = Bookmark.new(list_id: params[:list_id].to_i)
   end
 
   def create
     @bookmark = Bookmark.create(bookmark_params)
     if @bookmark
-      redirect_to bookmarks_path
+      redirect_to bookmark_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path, status: :see_other
   end
 
   private
@@ -18,6 +25,6 @@ class BookmarksController < ApplicationController
   end
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
